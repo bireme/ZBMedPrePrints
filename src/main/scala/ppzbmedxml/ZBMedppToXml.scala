85818baf6b4755ab1194a8fbc23f5f438ef4f441
michael-junior <michaeljunioroliveira@gmail.com>
2022-12-01 08:51:02 -0300

    change method strToSeq

--- src/main/scala/ppzbmedxml/ZBMedppToXml.scala
+++ src/main/scala/ppzbmedxml/ZBMedppToXml.scala
@@ -1,6 +1,6 @@
 package ppzbmedxml
 
-import org.mongodb.scala.bson.BsonArray
+import org.mongodb.scala.bson.{BsonArray, BsonValue}
 import org.mongodb.scala.Document
 
 import java.io.BufferedWriter
@@ -10,27 +10,27 @@ import scala.xml.{Elem, PrettyPrinter, XML}
 import scala.collection.JavaConverters._
 
 
-case class ZBMedPp_doc(id: String,
-                       db: String,
-                       instance: String,
-                       collection: String,
-                       pType: String,
-                       //la: String,                             ***Dado indisponível***
-                       pu: String,
-                       ti: String,
-                       doi: String,
-                       ur: String,
-                       urPdf: String,
-                       fulltext: String,
-                       ab: String,
-                       au: Seq[String],
-                       //afiliacaoAutor: String,                 ***Dado indisponível***
-                       entryDate: String,
-                       da: String)
-                       //versionMedrxivBiorxiv: String,          ***Dado indisponível***
-                       // license: String,                       ***Dado indisponível***
-                       //typeDocumentMedrxivBiorxiv: String,     ***Dado indisponível***
-                       //categoryMedrxivBiorxiv: String)         ***Dado indisponível***
+private case class ZBMedpp_doc(id: String,
+                               db: String,
+                               instance: String,
+                               collection: String,
+                               pType: String,
+                               //la: String,                             ***Dado indisponível***
+                               pu: String,
+                               ti: String,
+                               doi: String,
+                               ur: Seq[String],
+                               urPdf: Seq[String],
+                               fulltext: String,
+                               ab: String,
+                               au: Seq[String],
+                               //afiliacaoAutor: String,                 ***Dado indisponível***
+                               entryDate: String,
+                               da: String)
+                               //versionMedrxivBiorxiv: String,          ***Dado indisponível***
+                               // license: String,                       ***Dado indisponível***
+                               //typeDocumentMedrxivBiorxiv: String,     ***Dado indisponível***
+                               //categoryMedrxivBiorxiv: String)         ***Dado indisponível***
 
 
 class ZBMedPP{
@@ -38,13 +38,13 @@ class ZBMedPP{
   def toXml(docsMongo: Seq[Document], pathOut: String): Try[Unit] = {
     Try{
       generateXml(docsMongo.map(f => mapElements(f)), pathOut) match {
-        case Success(_) => println(s"\nLoading elements in the file generated in:: $pathOut")
+        case Success(_) => println(s"\nLoading elements in the file generated in: $pathOut")
         case Failure(_) => println(s"\nXml generation Failed!")
       }
     }
   }
 
-  def mapElements(doc: Document): ZBMedPp_doc ={
+  private def mapElements(doc: Document): ZBMedpp_doc ={
 
     val id: String = s"ppzbmed-${doc.getString("docLink").split("[/.]").reverse.head}"
     val bd: String = "PREPRINT-ZBMED"
@@ -54,18 +54,28 @@ class ZBMedPP{
     val pu: String = doc.getString("source")
     val ti: String = doc.getString("title").replace("<", "&lt;").replace(">","&gt;")
     val doi: String = doc.getString("id")
-    val link: String = doc.getString("link")
-    val linkPdf: String = doc.getString("pdfLink")
+    val link: Seq[String] = fieldToSeq(doc, "link")
+    val linkPdf: Seq[String] = fieldToSeq(doc, "pdfLink")
     val fullText: String = if (link.nonEmpty | linkPdf.nonEmpty) "1" else ""
     val ab: String = doc.getString("abstract").replace("<", "&lt;").replace(">","&gt;")
-    val au: Seq[String] = doc.get[BsonArray]("authors").get.getValues.asScala.map(tag => tag.asString().getValue).toSeq
+    val au: Seq[String] = fieldToSeq(doc, "authors")
     val entryDate: String = doc.getString("date").split("T").head.replace("-", "")
     val da: String = entryDate.substring(0,6)
 
-    ZBMedPp_doc(id, bd, instance, collection, typeTmp, pu, ti, doi, link, linkPdf, fullText, ab, au, entryDate, da)
+    ZBMedpp_doc(id, bd, instance, collection, typeTmp, pu, ti, doi, link, linkPdf, fullText, ab, au, entryDate, da)
   }
 
-  def generateXml(elements: Seq[ZBMedPp_doc], pathOut: String): Try[Unit] = {
+  private def fieldToSeq(doc: Document, nameField: String): Seq[String]={
+
+    doc.get[BsonValue](nameField).get match {
+      case field if field.isArray =>
+        doc.get[BsonArray](nameField).get.getValues.asScala.map(tag => tag.asString().getValue).toSeq
+      case field if field.isString => Seq(doc.getString(nameField))
+    }
+  }
+
+
+  private def generateXml(elements: Seq[ZBMedpp_doc], pathOut: String): Try[Unit] = {
     Try{
       val xmlPath: BufferedWriter = Files.newBufferedWriter(Paths.get(pathOut))
       val printer = new PrettyPrinter(80, 2)
@@ -80,7 +90,7 @@ class ZBMedPP{
     }
   }
 
-  def docToElem(fields: ZBMedPp_doc): Elem ={
+  private def docToElem(fields: ZBMedpp_doc): Elem ={
 
   <doc>
     <field name={"id"}>{fields.id}</field>
@@ -91,17 +101,17 @@ class ZBMedPP{
     <field name={"pu"}>{fields.pu}</field>
     <field name={"ti"}>{fields.ti}</field>
     <field name={"doi"}>{fields.doi}</field>
-    <field name={"ur"}>{fields.ur}</field>
-    <field name={"ur"}>{fields.urPdf}</field>
+    {fields.ur.map(f => setElement("ur", f))}
+    {fields.urPdf.map(f => setElement("ur", f))}
     <field name={"fulltext"}>{fields.fulltext}</field>
     <field name={"ab"}>{fields.ab}</field>
-    {fields.au.map(f => setAuthorsElem(f))}
+    {fields.au.map(f => setElement("au", f))}
     <field name={"entry_date"}>{fields.entryDate}</field>
     <field name={"da"}>{fields.da}</field>
   </doc>
   }
 
-  def setAuthorsElem(author: String): Elem = {
-    <field name={"au"}>{author}</field>
+  private def setElement(name: String, author: String): Elem = {
+    <field name={name}>{author}</field>
   }
 }
