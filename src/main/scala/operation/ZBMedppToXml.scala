2019f394ac274ff58a011b75896d617c3c11a116
Oliveira <oliveirmic@paho.org>
2023-08-10 17:15:40 -0300

    Correções nas tags do xml

--- src/main/scala/operation/ZBMedppToXml.scala
+++ src/main/scala/operation/ZBMedppToXml.scala
@@ -20,9 +20,7 @@ class ZBMedppToXml {
 
     var index = 0
     generateXml(docsMongo.map {
-     f =>
-
-        mapElements(f) match {
+     f => mapElements(f) match {
           case Success(value) =>
             index += 1
             amountProcessed(docsMongo.length, index, if (docsMongo.length >= 10000) 10000 else docsMongo.length)
@@ -30,7 +28,6 @@ class ZBMedppToXml {
           case Failure(exception) => throw new Exception(logger.error(s"_id Document in Mongodb: ${f.get("_id")} Exception: ", exception).toString)
         }
     }, pathOut)
-
   }
 
   private def mapElements(doc: Document): Try[ZBMedpp_doc] = {
@@ -135,7 +132,7 @@ class ZBMedppToXml {
     }
     if (h == null) ""
     else if (name == "all_annotations")
-      "^".concat(h)
+      "^d".concat(h)
     else h
   }
 
@@ -188,34 +185,31 @@ class ZBMedppToXml {
   private def docToElem(fields: ZBMedpp_doc): Elem = {
 
     //    {if (fields.urPdf.nonEmpty) {fields.urPdf.map(f => setElement("ur", f))} else xml.NodeSeq.Empty}
-    //{if (fields.au.nonEmpty) {fields.au.map(f => if (f.nonEmpty) setElement("au", f))} else xml.NodeSeq.Empty}
 
     <doc>
-      {if (fields.id.nonEmpty || fields.id != null) setElement("id", fields.id) else xml.NodeSeq.Empty}
-      {if (fields.alternateId.nonEmpty || fields.alternateId != null) setElement("alternate_id", fields.alternateId) else xml.NodeSeq.Empty}
-      {if (fields.dbSource.nonEmpty || fields.dbSource != null) setElement("db", fields.dbSource) else xml.NodeSeq.Empty}
-      {if (fields.instance.nonEmpty || fields.instance != null) setElement("instance", fields.instance) else xml.NodeSeq.Empty}
-      {if (fields.collection.nonEmpty || fields.collection != null) setElement("collection", fields.collection) else xml.NodeSeq.Empty}
-      {if (fields.pType.nonEmpty || fields.pType != null) setElement("type", fields.pType) else xml.NodeSeq.Empty}
-      {if (fields.la.nonEmpty || fields.la != null) setElement("la", fields.la) else xml.NodeSeq.Empty}
-      {if (fields.fo.nonEmpty || fields.fo != null) setElement("fo", fields.fo) else xml.NodeSeq.Empty}
-      {if (fields.dp.nonEmpty || fields.dp != null) setElement("dp", fields.dp) else xml.NodeSeq.Empty}
-      {if (fields.pu.nonEmpty || fields.pu != null) setElement("pu", fields.pu) else xml.NodeSeq.Empty}
-      {if (fields.ti.nonEmpty || fields.ti != null) setElement("ti", fields.ti) else xml.NodeSeq.Empty}
-      {if (fields.aid.nonEmpty || fields.aid != null) setElement("aid", fields.aid) else xml.NodeSeq.Empty}
-      {if (fields.ur.nonEmpty || fields.ur != null) fields.ur.map(f => if (f.nonEmpty) setElement("ur", f)) else xml.NodeSeq.Empty}
-      {if (fields.fulltext.nonEmpty || fields.fulltext != null) setElement("fulltext", fields.fulltext) else xml.NodeSeq.Empty}
-      {if (fields.ab.nonEmpty || fields.ab != null) setElement("ab", fields.ab) else xml.NodeSeq.Empty}
-      {if (fields.au.nonEmpty || fields.au != null) {fields.au.map(f => if (f.nonEmpty) setElement("au", f))} else xml.NodeSeq.Empty}
-      {if (fields.entryDate.nonEmpty || fields.entryDate != null) setElement("entry_date", fields.entryDate) else xml.NodeSeq.Empty}
-      {if (fields.da.nonEmpty || fields.da != null) setElement("da", fields.da) else xml.NodeSeq.Empty}
-      {if (fields.mj.nonEmpty || fields.mj != null) fields.mj.map(f => if (f.nonEmpty & f != null) setElement("mj", f)) else xml.NodeSeq.Empty}
+      {if (fields.id.nonEmpty) setElement("id", fields.id) else xml.NodeSeq.Empty}
+      {if (fields.alternateId.nonEmpty) setElement("alternate_id", fields.alternateId) else xml.NodeSeq.Empty}
+      {if (fields.dbSource.nonEmpty) setElement("db", fields.dbSource) else xml.NodeSeq.Empty}
+      {if (fields.instance.nonEmpty) setElement("instance", fields.instance) else xml.NodeSeq.Empty}
+      {if (fields.collection.nonEmpty) setElement("collection", fields.collection) else xml.NodeSeq.Empty}
+      {if (fields.pType.nonEmpty) setElement("type", fields.pType) else xml.NodeSeq.Empty}
+      {if (fields.la.nonEmpty) setElement("la", fields.la) else xml.NodeSeq.Empty}
+      {if (fields.fo.nonEmpty) setElement("fo", fields.fo) else xml.NodeSeq.Empty}
+      {if (fields.dp.nonEmpty) setElement("dp", fields.dp) else xml.NodeSeq.Empty}
+      {if (fields.pu.nonEmpty) setElement("pu", fields.pu) else xml.NodeSeq.Empty}
+      {if (fields.ti.nonEmpty) setElement("ti", fields.ti) else xml.NodeSeq.Empty}
+      {if (fields.aid.nonEmpty) setElement("aid", fields.aid) else xml.NodeSeq.Empty}
+      {if (fields.ur.nonEmpty) {fields.ur.map(f => if (f.nonEmpty) setElement("ur", f))} else xml.NodeSeq.Empty}
+      {if (fields.fulltext.nonEmpty) setElement("fulltext", fields.fulltext) else xml.NodeSeq.Empty}
+      {if (fields.ab.nonEmpty) setElement("ab", fields.ab) else xml.NodeSeq.Empty}
+      {if (fields.au.nonEmpty) {fields.au.map(f => if (f.nonEmpty) setElement("au", f))} else xml.NodeSeq.Empty}
+      {if (fields.entryDate.nonEmpty) setElement("entry_date", fields.entryDate) else xml.NodeSeq.Empty}
+      {if (fields.da.nonEmpty) setElement("da", fields.da) else xml.NodeSeq.Empty}
+      {if (fields.mj.nonEmpty) {fields.mj.map(f => if (f.nonEmpty) setElement("mj", f))} else xml.NodeSeq.Empty}
     </doc>
   }
 
-  private def setElement(name: String, field: String): Elem = <field name={name}>
-    {field}
-  </field>
+  private def setElement(name: String, field: String): Elem = <field name={name}>{field}</field>
 
   def amountProcessed(total: Int, qtdProccess: Int, setAmount: Int): Unit = {
     val valuePrints: Double = total / setAmount
